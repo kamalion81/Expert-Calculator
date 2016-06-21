584f5bd733e0171476da30d46907f13a671b3e07
kamalion <kamalion81@gmail.com>
2016-06-21 18:03:25 +0300

    Добавлен.

---
+++ src/main/java/Calc.java
@@ -0,0 +1,371 @@
+import java.io.Serializable;
+ 
+public class Calc implements Serializable {
+ 
+    private int steel;
+    private int intPressure;
+    private int hydroPressure;
+    private int temp;
+    private int tempT;
+    private int temp20;
+    private int diam;
+    private int thickness;
+    private int corrosion;
+    private int minusTolerance;
+    private int techno;
+    private int addThickness;
+    private int lineExpCoeff;
+    private int elasticity;
+    private int yieldStrength;
+    private int tempResist;
+    private int yieldStrength20;
+    private int weld;
+    private int bending;
+    private int shift;
+    private int force;
+    private int calcThickness;
+    private String greaterPressure;
+    private String greaterThickness;
+    
+    
+    
+    
+    
+
+    /**
+     * @return the steel
+     */
+    public int getSteel() {
+        return steel;
+    }
+
+    /**
+     * @param steel the steel to set
+     */
+    public void setSteel(int steel) {
+        this.steel = steel;
+    }
+
+    /**
+     * @return the intPressure
+     */
+    public int getIntPressure() {
+        return intPressure;
+    }
+
+    /**
+     * @param intPressure the intPressure to set
+     */
+    public void setIntPressure(int intPressure) {
+        this.intPressure = intPressure;
+    }
+
+    /**
+     * @return the hydroPressure
+     */
+    public int getHydroPressure() {
+        return hydroPressure;
+    }
+
+    /**
+     * @param hydroPressure the hydroPressure to set
+     */
+    public void setHydroPressure(int hydroPressure) {
+        this.hydroPressure = hydroPressure;
+    }
+
+    /**
+     * @return the temp
+     */
+    public int getTemp() {
+        return temp;
+    }
+
+    /**
+     * @param temp the temp to set
+     */
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    /**
+     * @return the tempT
+     */
+    public int getTempT() {
+        return tempT;
+    }
+
+    /**
+     * @param tempT the tempT to set
+     */
+    public void setTempT(int tempT) {
+        this.tempT = tempT;
+    }
+
+    /**
+     * @return the temp20
+     */
+    public int getTemp20() {
+        return temp20;
+    }
+
+    /**
+     * @param temp20 the temp20 to set
+     */
+    public void setTemp20(int temp20) {
+        this.temp20 = temp20;
+    }
+
+    /**
+     * @return the diam
+     */
+    public int getDiam() {
+        return diam;
+    }
+
+    /**
+     * @param diam the diam to set
+     */
+    public void setDiam(int diam) {
+        this.diam = diam;
+    }
+
+    /**
+     * @return the thickness
+     */
+    public int getThickness() {
+        return thickness;
+    }
+
+    /**
+     * @param thickness the thickness to set
+     */
+    public void setThickness(int thickness) {
+        this.thickness = thickness;
+    }
+
+    /**
+     * @return the corrosion
+     */
+    public int getCorrosion() {
+        return corrosion;
+    }
+
+    /**
+     * @param corrosion the corrosion to set
+     */
+    public void setCorrosion(int corrosion) {
+        this.corrosion = corrosion;
+    }
+
+    /**
+     * @return the minusTolerance
+     */
+    public int getMinusTolerance() {
+        return minusTolerance;
+    }
+
+    /**
+     * @param minusTolerance the minusTolerance to set
+     */
+    public void setMinusTolerance(int minusTolerance) {
+        this.minusTolerance = minusTolerance;
+    }
+
+    /**
+     * @return the techno
+     */
+    public int getTechno() {
+        return techno;
+    }
+
+    /**
+     * @param techno the techno to set
+     */
+    public void setTechno(int techno) {
+        this.techno = techno;
+    }
+
+    /**
+     * @return the addThickness
+     */
+    public int getAddThickness() {
+        return addThickness;
+    }
+
+    /**
+     * @param addThickness the addThickness to set
+     */
+    public void setAddThickness(int addThickness) {
+        this.addThickness = addThickness;
+    }
+
+    /**
+     * @return the lineExpCoeff
+     */
+    public int getLineExpCoeff() {
+        return lineExpCoeff;
+    }
+
+    /**
+     * @param lineExpCoeff the lineExpCoeff to set
+     */
+    public void setLineExpCoeff(int lineExpCoeff) {
+        this.lineExpCoeff = lineExpCoeff;
+    }
+
+    /**
+     * @return the elasticity
+     */
+    public int getElasticity() {
+        return elasticity;
+    }
+
+    /**
+     * @param elasticity the elasticity to set
+     */
+    public void setElasticity(int elasticity) {
+        this.elasticity = elasticity;
+    }
+
+    /**
+     * @return the yieldStrength
+     */
+    public int getYieldStrength() {
+        return yieldStrength;
+    }
+
+    /**
+     * @param yieldStrength the yieldStrength to set
+     */
+    public void setYieldStrength(int yieldStrength) {
+        this.yieldStrength = yieldStrength;
+    }
+
+    /**
+     * @return the tempResist
+     */
+    public int getTempResist() {
+        return tempResist;
+    }
+
+    /**
+     * @param tempResist the tempResist to set
+     */
+    public void setTempResist(int tempResist) {
+        this.tempResist = tempResist;
+    }
+
+    /**
+     * @return the yieldStrength20
+     */
+    public int getYieldStrength20() {
+        return yieldStrength20;
+    }
+
+    /**
+     * @param yieldStrength20 the yieldStrength20 to set
+     */
+    public void setYieldStrength20(int yieldStrength20) {
+        this.yieldStrength20 = yieldStrength20;
+    }
+
+    /**
+     * @return the weld
+     */
+    public int getWeld() {
+        return weld;
+    }
+
+    /**
+     * @param weld the weld to set
+     */
+    public void setWeld(int weld) {
+        this.weld = weld;
+    }
+
+    /**
+     * @return the bending
+     */
+    public int getBending() {
+        return bending;
+    }
+
+    /**
+     * @param bending the bending to set
+     */
+    public void setBending(int bending) {
+        this.bending = bending;
+    }
+
+    /**
+     * @return the shift
+     */
+    public int getShift() {
+        return shift;
+    }
+
+    /**
+     * @param shift the shift to set
+     */
+    public void setShift(int shift) {
+        this.shift = shift;
+    }
+
+    /**
+     * @return the force
+     */
+    public int getForce() {
+        return force;
+    }
+
+    /**
+     * @param force the force to set
+     */
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    /**
+     * @return the calcThickness
+     */
+    public int getCalcThickness() {
+        return calcThickness;
+    }
+
+    /**
+     * @param calcThickness the calcThickness to set
+     */
+    public void setCalcThickness(int calcThickness) {
+        this.calcThickness = calcThickness;
+    }
+
+    /**
+     * @return the greaterPressure
+     */
+    public String getGreaterPressure() {
+        return greaterPressure;
+    }
+
+    /**
+     * @param greaterPressure the greaterPressure to set
+     */
+    public void setGreaterPressure(String greaterPressure) {
+        this.greaterPressure = greaterPressure;
+    }
+
+    /**
+     * @return the greaterThickness
+     */
+    public String getGreaterThickness() {
+        return greaterThickness;
+    }
+
+    /**
+     * @param greaterThickness the greaterThickness to set
+     */
+    public void setGreaterThickness(String greaterThickness) {
+        this.greaterThickness = greaterThickness;
+    }
+     
+}
